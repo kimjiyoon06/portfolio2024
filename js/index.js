fa5b0616e2179af06126d2b71fecb28b19887122
kimjiyoon06 <jiji2306@naver.com>
2024-01-29 14:08:52 +0900

    모달까지 수정

--- js/index.js
+++ js/index.js
@@ -14,75 +14,150 @@ window.onload = function(){
         })
     })
     
-    let workData;
-    const eventXhttp = new XMLHttpRequest();
-    eventXhttp.open("GET", "js/workdata.json");
-    eventXhttp.send();
-    eventXhttp.onreadystatechange = function(event){
-        const req = event.target;
-        if(req.readyState === XMLHttpRequest.DONE){
-            workData = JSON.parse(req.response);
-            workSection(workData)
-        }
-    }
-    function workSection(_data){
-        let tabList = document.querySelector(".tab-list")
-        workData = _data;
-        let tabHtml = ``;
-        let dataArr = _data.work;
-        for(let i = 0; i < dataArr.length; i++){
-            let html = `<li>${dataArr[i].catename}</li>`
-            tabHtml += html
-         }
-         tabList.innerHTML = tabHtml
-         
-         let tabItem = document.querySelectorAll(".tab-list li")
-         for(let i = 0; i< dataArr.length; i++){
-            tabItem[0].classList.add("active")
-            tabItem[i].addEventListener("click", function(){
-                for(let j = 0; j < tabItem.length; j++){
-                    tabItem[j].classList.remove("active")
-                }
-                tabItem[i].classList.add("active")
-                workSlide(i)
-            })
-         }
-         workSlide(0)
 
-    }
-    let workswiper;
-    function workSlide(_idx){
-        let swWorkhtml = ``
-        let listData = workData.work[_idx].list;
-        for(let i = 0; i<listData.length; i++){
-            let obj = listData[i];
-            let html = `
-                <li class="swiper-slide">
-                    <div class="textbox">
-                        <h1>${obj.title}</h1>
-                        <h2>${obj.text}<h2>
-                        <p>작업기간 : ${obj.period}</p>
-                        
-                    </div>
-                    <div class="imgbox">
-                        <img src="https://img.youtube.com/vi/${obj.vidoid}/maxresdefault.jpg" alt="${obj.alt}">
-                    </div>
+
+
+
+        let modal = document.querySelector(".modal")
+        let modalCont = document.querySelector(".modal-cont")
+        let body = document.querySelector("body")
+        let workData;
+        const workXttp = new XMLHttpRequest();
+        workXttp.open("GET", "js/workdata.json");
+        workXttp.send();
+        workXttp.onreadystatechange = function(event){
+            const req = event.target;
+            if(req.readyState === XMLHttpRequest.DONE){
+                workData = JSON.parse(req.response);
+                parseWork(workData);
+            }
+        };
+
+        function parseWork(_data){
+            let workCate = document.querySelector(".portfolio .tab-list");
+            workData = _data;
+            let tabHtml = ``;
+            let dataArr = _data.work;
+            for(let i = 0; i < dataArr.length; i++){
+                let html = `<li><a href="#">${dataArr[i].catename}</a></li>`;
+                tabHtml += html;
+            }
+            workCate.innerHTML = tabHtml;
+
+            let tabs = document.querySelectorAll(".portfolio .tab-list li");
+            for(let i = 0; i < dataArr.length; i++){
+                tabs[0].classList.add("active");
+                console.log(tabs[i]);
+                tabs[i].addEventListener("click",function(event){
+                    event.preventDefault();
+                    workSlide(i)
+                    for(let j = 0; j<tabs.length; j++){
+                        tabs[j].classList.remove("active");
+                    }
+                    this.classList.add("active")
                     
-                </li> 
-            `;
-            swWorkhtml += html 
-
+                })
+                
+            }
+            workSlide(0)
+            
         }
-        let swWorkWrapper = document.querySelector(".sw-work ul")
-        swWorkWrapper.innerHTML = swWorkhtml
+        let workSwiper;
+        function workSlide(_idx){
+            let swworkHtml = ``;
+            let listData = workData.work[_idx].list;
+            for(let i = 0; i < listData.length; i++){
+                let obj = listData[i];
+                let html = `
+                    <li class="swiper-slide">
+                        
+                        <div class="txtbox">
+                            <h1 class="title">${obj.title}</h1>
+                            <h2 class="text">${obj.text}</h2>
+                            <p class="period" 
+                                ${obj.period ? "style='display:block'" : "style='display:none'"}>
+                                ${obj.period}<br><span>${obj.period2}</span>
+                            </p>
+                            <div class="skills">
+                                <h3>사용툴</h3>
+                                <ul class="skill-wrap">
+                                    <li ${obj.afterEffect ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="../img/folder_after_effects.svg" alt="">
+                                    </li>
+                                    <li ${obj.premiere ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="../img/folder_premiere.svg" alt="">
+                                    </li>
+                                    <li ${obj.css ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="../img/folder_css.svg" alt="">
+                                    </li>
+                                    <li ${obj.html ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="../img/folder_html.svg" alt="">
+                                    </li>
+                                    <li ${obj.illust ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="../img/folder_illust.svg" alt="">
+                                    </li>
+                                    <li ${obj.photoshop ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="img/Adobe-Photoshop-img.png" alt="">
+                                    </li>
+                                    <li ${obj.git ? "style='display:block'" : "style='display:none'"}>
+                                        <img src="img.svg" alt="">
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="right-cont">
+                            <div class="imgbox">
+                                <img src="https://img.youtube.com/vi/${obj.videoid}/maxresdefault.jpg" alt="${obj.alt}" ${obj.videoid ? "style='display:block'" : "style='display:none'"}>
+                                <img src="img/${obj.imgurl}" alt="${obj.alt}" ${obj.imgurl ? "style='display:block'" : "style='display:none'"}>
+                            </div>
+                            <div class="view-btn">
+                                <p>VIEW</p>
+                            </div>
+                        </div>
+                    </li>
+                `;
+                swworkHtml += html;
+            }
+            let swworkWrapper = document.querySelector(".sw-work .swiper-wrapper");
+            swworkWrapper.innerHTML = swworkHtml;
+            
+            if(workSwiper){
+                workSwiper.destroy();
+            }
+            workSwiper = new Swiper(".sw-work", {
+                slidesPerView: 1,
+            })
 
-        workswiper = new Swiper(".sw-work",{
-            slidesPerView: 1,
-            spaceBetween: 15,
-            // effect:"fade"
-        })
-
-    }
+            // 썸네일 클릭 > 모달 오픈
+            
+            let workItem = document.querySelectorAll(".sw-work li")
+            workItem.forEach(function(item, index){
+                item.addEventListener("click", function(){
+                    let obj = workData.work[_idx].list[index];
+                    modal.classList.add("active")
+                    modalCont.innerHTML = `
+                        <div class="view-img" ${obj.imgurl ? "style='display:block'" : "style='display:none'"}>
+                            <img src="img/${obj.imgurl}" alt="">
+                        </div>
+                        <div class="view-player" ${obj.videoid ? "style='display:block'" : "style='display:none'"}>
+                            <iframe src="https://www.youtube.com/embed/${obj.videoid}" allowfullscreen></iframe>
+                        </div>
+                    `
+                    setTimeout(function(){
+                        modalCont.classList.add("active")
+                    },500)
+                    body.classList.add("scrollfix")
+                })
+            })
+            modal.addEventListener("click", function(){
+                modal.classList.remove("active")
+                modalCont.classList.remove("active")
+                body.classList.remove("scrollfix")
+                modalCont.innerHTML = ``
+            })
+        }
+    
+    
 }
 
 
